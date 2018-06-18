937cffd2015abfce0816e345ac1439daa110a7f0
Ian Talisic <ianfreelancemail@gmail.com>
2018-06-19 00:09:18 +0800

    Fixed Login Button appearance
    Signed-off-by: Ian Talisic <ianfreelancemail@gmail.com>

--- mysampleapp/lib/screens/login/login_screen.dart
+++ mysampleapp/lib/screens/login/login_screen.dart
@@ -17,19 +17,26 @@ class LoginFormState extends State<LoginForm>{
       backgroundColor: Colors.pinkAccent,
       body: new Center (
         child: new Container(
-          height: 300.0,
+          constraints: BoxConstraints(
+          maxHeight: 350.0,
+          maxWidth: 350.0,
+          minWidth: 250.0,
+          minHeight: 250.0
+      ),
           alignment:  Alignment.center,
-          color: Colors.lightBlue,
-          padding: new EdgeInsets.all(30.0),
+          color: Colors.white24,
+          padding: new EdgeInsets.symmetric(horizontal: 30.0),
           child: new Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              new Image.network(
-                'https://raw.githubusercontent.com/flutter/website/master/_includes/code/layout/lakes/images/lake.jpg',
-                height: 50.0,
+              new Padding(
+                padding: new EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 10.0),
+                child:  new Image.network(
+                  'https://raw.githubusercontent.com/flutter/website/master/_includes/code/layout/lakes/images/lake.jpg',
+                  height: 50.0,
+                ),
               ),
               new Padding(
-                padding: new EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 5.0),
+                padding: new EdgeInsets.fromLTRB(30.0, 5.0, 30.0, 5.0),
                 child: new TextField(
                     keyboardType: TextInputType.emailAddress,
                     textAlign: TextAlign.center,
@@ -51,22 +58,48 @@ class LoginFormState extends State<LoginForm>{
                   ),
                 ),
               ),
-              new InkWell(
-                onTap: (){
-                  Navigator.pushNamed(context, '/home');
-                },
-                child: new Container(
-                  //width: 100.0,
-                  padding: new EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 5.0),
-                  height: 50.0,
-                  decoration: new BoxDecoration(
-                    color: Colors.blueAccent,
-                    border: new Border.all(color: Colors.white, width: 2.0),
-                    borderRadius: new BorderRadius.circular(10.0),
+              new Padding (
+                 padding: new EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 5.0),
+                child:  new InkWell(
+                  onTap: (){
+                    Navigator.pushNamed(context, '/home');
+                  },
+                  child: new Container(
+                    //width: 100.0,
+               margin: new  EdgeInsets.only(
+                 left: 30.0,
+                 top: 30.0,
+                 right: 30.0,
+                 bottom: 5.0
+               ),
+                    height: 50.0,
+                    decoration: new BoxDecoration(
+                      color: Colors.blueAccent,
+                      border: new Border.all(color: Colors.white, width: 2.0),
+                      borderRadius: new BorderRadius.circular(25.0),
+                    ),
+                    child: new Center(child: new Text('Login', style: new TextStyle(fontSize: 18.0, color: Colors.white),),),
                   ),
-                  child: new Center(child: new Text('Click Me', style: new TextStyle(fontSize: 18.0, color: Colors.white),),),
+                )
+              ),
+              new Padding(
+                padding: new EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 10.0),
+                child: new Container(
+                  color: Colors.black,
+                  height: 50.0,
+                  // decoration:new  BoxDecoration(
+                  //     color: Colors.blueAccent,
+                  //     border: new Border.all(color: Colors.white, width: 2.0),
+                  //     borderRadius: new BorderRadius.circular(25.0),
+                  // ),
+                  child: new TextField(
+                    textAlign: TextAlign.center,
+                    decoration: new InputDecoration(
+                      hintText: "Username",
+                    ),
+                  ),
                 ),
-              )
+              ),
             ],
           ),
         ),
